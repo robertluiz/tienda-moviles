eeaed11262b6f2545ed8c1898f2f8f223fd4aa76
Robert Luiz <robert.tiweb@gmail.com>
2025-04-25 13:24:14 +0200

    Actualiza index.html y vite.config.ts: añade meta de Permissions-Policy y ajusta la base para entornos de GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tienda-moviles',
+  base: process.env.GITHUB_PAGE === 'true' ? '/tienda-moviles' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),

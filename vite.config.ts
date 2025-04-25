f9e8090491f48017ec69fbac327035dd30ce1da0
Robert Luiz <robert.tiweb@gmail.com>
2025-04-25 13:15:27 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/tienda-moviles',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
